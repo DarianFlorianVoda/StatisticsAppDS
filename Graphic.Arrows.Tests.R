7d3bfa29f4552da749a28740970c6fbd403f5a9c
MasterDari <73436590+DarianFlorianVoda@users.noreply.github.com>
2022-01-27 22:31:02 +0200

    Added Comments & Perturbed slopes

--- Graphic.Arrows.Tests.R
+++ Graphic.Arrows.Tests.R
@@ -138,9 +138,12 @@ slope = compute_slope(x, y);
 # Note: x = (x0, x1); y = (y0, y1);
 # Testing various origins
 
+### Test: Lines
+
+### Test Different lines on x-axis where x is between 0 and 10
+### Point P is the upper limit of the line with x2 = 10; y2 = 5
 P = c(10,5)
 
-### x0 = [0, 10]; y0 = 0
 plot.base()
 x = c(0,P[1]); y = c(0, P[2]);
 lines(x, y, lwd=2, col="red") 
@@ -176,7 +179,7 @@ x = c(10,P[1]); y = c(0, P[2]);
 lines(x, y, lwd=2, col="red")
 
 
-### x0 = 0; y0 = [0,10]
+### Test Different lines on x-axis where y is between 0 and 10
 plot.base(xlim=c(-2,10), ylim=c(-2,10), axt=c(2))
 x = c(0,10); y = c(0, 5);
 lines(x, y, lwd=2, col="red") # Why is it right?
@@ -211,24 +214,23 @@ lines(x, y, lwd=2, col="red")
 x = c(0,P[1]); y = c(10, P[2]);
 lines(x, y, lwd=2, col="red")
 
-### Horizontal
+### -- origin: (0, 0), (0, 1), (0, 2), (0, -2), ... ;
+# line
 plot.base(xlim=c(-2,10), ylim=c(-2,10), axt=c(1,2))
 x = c(0,5); y = c(4, 4);
-lines(x, y, lwd=2, col="red") # Why is it right?
+lines(x, y, lwd=2, col="red")
 abline(v=x, col="green", lty=3)
 
-p0 = c(2, 6)
+# points(x, y)!
+# Test 1
+p0 = c(0, 0)
 p = reflect(x, y, p0)
 points(p0[1], p0[2]); points(p[1], p[2]);
 lines(c(p0[1], p[1]), c(p0[2], p[2]), col="green")
 
-### -- origin: (0, 0), (0, 1), (0, 2), (0, -2), ...;
-plot.base()
-P = c(0, 0)
-x = c(0,P[1]); y = c(0, P[2]);
-lines(x, y, lwd=2, col="red")
 
 ### Shift point along line:
+
 p = shiftH(c(0,1), x, y, d=1)
 points(p, col="green")
 # only as example:
@@ -260,20 +262,25 @@ l = shift(x, y, d=2)
 len = nrow(l) / 2;
 sapply(seq(len), function(id) lines(l[c(id, id+len),1], l[c(id, id+len),2], col="orange"))
 
-### Horizontal slope
+### Test: Reflections
+
+### Horizontal Reflection
+
+# line
 plot.base(xlim=c(-2,10), ylim=c(-2,10), axt=c(1,2))
 x = c(0,5); y = c(4, 4);
 lines(x, y, lwd=2, col="red")
 abline(v=x, col="green", lty=3)
 
+# points(x, y)!
+# Test 1
 p0 = c(2, 6)
 p = reflect(x, y, p0)
-points(p0[1], p0[2])
-points(p[1], p[2])
+points(p0[1], p0[2]); points(p[1], p[2]);
 lines(c(p0[1], p[1]), c(p0[2], p[2]), col="green")
-stopifnot(all(p == c(2,2)))
+stopifnot(all(p==c(2,2)))
 
-
+# Test 2
 p0 = c(1, 8)
 p = reflect(x, y, p0)
 points(p0[1], p0[2])
@@ -281,6 +288,7 @@ points(p[1], p[2])
 lines(c(p0[1], p[1]), c(p0[2], p[2]), col="green")
 stopifnot(all(p == c(1,0)))
 
+# Test 3
 p0 = c(3, 5)
 p = reflect(x, y, p0)
 points(p0[1], p0[2])
@@ -288,6 +296,7 @@ points(p[1], p[2])
 lines(c(p0[1], p[1]), c(p0[2], p[2]), col="green")
 stopifnot(all(p == c(3,3)))
 
+# Test 4
 p0 = c(4, 9)
 p = reflect(x, y, p0)
 points(p0[1], p0[2])
@@ -295,10 +304,8 @@ points(p[1], p[2])
 lines(c(p0[1], p[1]), c(p0[2], p[2]), col="green")
 stopifnot(all(p == c(4,-1)))
 
-# Question: Should add all into one p to check at the final?
 
-
-### Vertical Slope
+### Vertical Reflection
 
 # line
 plot.base(xlim=c(-2,10), ylim=c(-2,10), axt=c(1,2))
@@ -307,6 +314,7 @@ lines(x, y, lwd=2, col="red")
 abline(v=c(-2,8), col="green", lty=3)
 
 # points(x, y)!
+# Test 1
 p0 = c(4, 7)
 p = reflect(x, y, p0)
 points(p0[1], p0[2]) 
@@ -314,6 +322,7 @@ points(p[1], p[2])
 lines(c(p0[1], p[1]), c(p0[2], p[2]), col="green")
 stopifnot(all(p == c(2,7)))
 
+# Test 2
 p0 = c(6, 5)
 p = reflect(x, y, p0)
 points(p0[1], p0[2]) 
@@ -321,6 +330,7 @@ points(p[1], p[2])
 lines(c(p0[1], p[1]), c(p0[2], p[2]), col="green")
 stopifnot(all(p == c(0,5)))
 
+# Test 3
 p0 = c(8, 3)
 p = reflect(x, y, p0)
 points(p0[1], p0[2]) 
@@ -329,13 +339,15 @@ lines(c(p0[1], p[1]), c(p0[2], p[2]), col="green")
 stopifnot(all(p == c(-2,3)))
 
 
-### Ascending Slope
+### Ascending Reflection
+
 # line
 plot.base(xlim=c(-2,10), ylim=c(-2,10), axt=c(1,2))
 x = c(0,6); y = c(0, 6);
 lines(x, y, lwd=2, col="red")
 
 # points(x, y)!
+# Test 1
 p0 = c(1, 2)
 p = reflect(x, y, p0)
 points(p0[1], p0[2]) 
@@ -343,6 +355,7 @@ points(p[1], p[2])
 lines(c(p0[1], p[1]), c(p0[2], p[2]), col="green")
 stopifnot(all(p == c(2,1)))
 
+# Test 2
 p0 = c(2, 4)
 p = reflect(x, y, p0)
 points(p0[1], p0[2]) 
@@ -350,6 +363,7 @@ points(p[1], p[2])
 lines(c(p0[1], p[1]), c(p0[2], p[2]), col="green")
 stopifnot(all(p == c(4,2)))
 
+# Test 3
 p0 = c(4, 7)
 p = reflect(x, y, p0)
 points(p0[1], p0[2]) 
@@ -358,31 +372,109 @@ lines(c(p0[1], p[1]), c(p0[2], p[2]), col="green")
 stopifnot(all(p == c(7,4)))
 
 
-### Descending Slope
+### Descending Reflection
+
 # line
 plot.base(xlim=c(-2,10), ylim=c(-2,10), axt=c(1,2))
 x = c(0,8); y = c(8, 1);
 lines(x, y, lwd=2, col="red")
 
 # points(x, y)!
+# Test 1
 p0 = c(1, 8)
 p = reflect(x, y, p0)
 points(p0[1], p0[2]) 
 points(p[1], p[2])
 lines(c(p0[1], p[1]), c(p0[2], p[2]), col="green")
-# p has float values stopifnot(all(p == c(0.133,7.009)))
+# Ambiguity: p has float values stopifnot(all(p == c(0.133,7.009)))
 
+# Test 2
 p0 = c(3, 8)
 p = reflect(x, y, p0)
 points(p0[1], p0[2]) 
 points(p[1], p[2])
 lines(c(p0[1], p[1]), c(p0[2], p[2]), col="green")
-# p has float values stopifnot(all(p == c(0.133,7.009)))
+# Ambiguity: p has float values stopifnot(all(p == c(0.133,7.009)))
 
 p0 = c(6, 7)
 p = reflect(x, y, p0)
 points(p0[1], p0[2]) 
 points(p[1], p[2])
 lines(c(p0[1], p[1]), c(p0[2], p[2]), col="green")
-# p has float values stopifnot(all(p == c(0.133,7.009))) ??
+# Ambiguity: p has float values stopifnot(all(p == c(0.133,7.009))) ??
+
+
+### Slightly Perturbed Vertical Reflection
+
+# line
+plot.base(xlim=c(-2,10), ylim=c(-2,10), axt=c(1,2))
+x = c(3.25,3); y = c(1, 8);
+lines(x, y, lwd=2, col="red")
+abline(v=c(-2,8), col="green", lty=3)
+
+# points(x, y)!
+# Test 1
+p0 = c(4, 7)
+p = reflect(x, y, p0)
+points(p0[1], p0[2]) 
+points(p[1], p[2])
+lines(c(p0[1], p[1]), c(p0[2], p[2]), col="green")
+# Ambugity: stopifnot(all(p == c(2.073885,6.931210)))
+
+# Test 2
+p0 = c(6, 5)
+p = reflect(x, y, p0)
+points(p0[1], p0[2]) 
+points(p[1], p[2])
+lines(c(p0[1], p[1]), c(p0[2], p[2]), col="green")
+# Ambiguity: stopifnot(all(p == c(0,5)))
+
+# Test 3
+p0 = c(8, 3)
+p = reflect(x, y, p0)
+points(p0[1], p0[2]) 
+points(p[1], p[2])
+lines(c(p0[1], p[1]), c(p0[2], p[2]), col="green")
+# Ambiguity: stopifnot(all(p == c(-2,3)))
+
+
+### Slightly Perturbed Horizontal Reflection
+
+# line
+plot.base(xlim=c(-2,10), ylim=c(-2,10), axt=c(1,2))
+x = c(0,5); y = c(4, 4.5);
+lines(x, y, lwd=2, col="red")
+abline(v=x, col="green", lty=3)
+
+# points(x, y)!
+# Test 1
+p0 = c(2, 6)
+p = reflect(x, y, p0)
+points(p0[1], p0[2]); points(p[1], p[2]);
+lines(c(p0[1], p[1]), c(p0[2], p[2]), col="green")
+# Ambiguity: stopifnot(all(p==c(2,2)))
+
+# Test 2
+p0 = c(1, 8)
+p = reflect(x, y, p0)
+points(p0[1], p0[2])
+points(p[1], p[2])
+lines(c(p0[1], p[1]), c(p0[2], p[2]), col="green")
+# Ambiguity: stopifnot(all(p == c(1,0)))
+
+# Test 3
+p0 = c(3, 5)
+p = reflect(x, y, p0)
+points(p0[1], p0[2])
+points(p[1], p[2])
+lines(c(p0[1], p[1]), c(p0[2], p[2]), col="green")
+# Ambiguity: stopifnot(all(p == c(3,3)))
+
+# Test 4
+p0 = c(4, 9)
+p = reflect(x, y, p0)
+points(p0[1], p0[2])
+points(p[1], p[2])
+lines(c(p0[1], p[1]), c(p0[2], p[2]), col="green")
+# Ambiguity: stopifnot(all(p == c(4,-1)))
 
