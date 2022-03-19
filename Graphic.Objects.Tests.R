15f50f57d241bb9ce8b13d080ca44d42a426d435
DarianV <darian.voda00@e-uvt.ro>
2022-03-19 19:47:41 +0200

    -Added modified comments from coordinator

--- Graphic.Objects.Tests.R
+++ Graphic.Objects.Tests.R
@@ -35,7 +35,7 @@ testFilledCircle = function(xy, r=1, R=NULL, d=NULL, col="#B0B032", line=TRUE, a
     if(is.null(col)) {
       filledcircle(r1=r, r2=0, mid = c(x[id], y[id]))
     } else {
-        filledcircle(r1=r, r2=0, mid = c(x[id], y[id]), col=col);
+      filledcircle(r1=r, r2=0, mid = c(x[id], y[id]), col=col);
     }
   }
   if(line) {
@@ -55,7 +55,9 @@ phi = pi / n;
 xy = pointsCircle(n, R, phi=phi);
 testFilledCircle(xy, d=R+1);
 
+
 #### Closed Circles ####
+# - n known;
 n = 15
 r = 1
 phi = pi / n; # add some rotation
@@ -63,13 +65,25 @@ xy = circlesOnCircle(n, r, phi=phi);
 testFilledCircle(xy);
 
 # Radius of Big Circle: known
+# - small circles: on the big circle;
 R = 7
 xy = circlesOnFixedCircle(n, r=R, phi=phi);
 testFilledCircle(xy, R=R);
 
+
 #### Inside Circle ####
 
-### Outer Circle
+### Example 1: Simple
+n = 19
+R = 15
+phi = pi / n; # add some rotation
+xy = circlesInFixedCircle(n, R, phi=phi);
+testFilledCircle(xy, R=R)
+
+
+### Example 2:
+# Tangent & Inside given Circle
+# Outer Circle: known
 n = 19
 r = 1
 phi = pi / n; # add some rotation
@@ -79,17 +93,24 @@ testFilledCircle(xy)
 # Inner Circle: unknown
 R = attr(xy, "R") - r;
 xy = circlesInFixedCircle(n, r=R, phi=phi);
-testFilledCircle(xy, add=TRUE);
+testFilledCircle(xy, add=TRUE, line=FALSE);
+
 
 #### Outside Circle ####
-###
+
+# Tangent & Outside given Circle
 n = 13
 R = 6
 phi = pi / n; # add some rotation
 xy = circlesOutsideFixedCircle(n, R, phi=phi);
 testFilledCircle(xy, R=R);
 
-#### Smooth Muscles / Connective Tissue ####
+
+#####################
+
+#### Cell Shapes ####
+
+### Smooth Muscles / Connective Tissue
 plot.base()
 lst = cellSmooth(c(1,5), c(2, 7))
 lines.object.base(lst, lwd=2)
