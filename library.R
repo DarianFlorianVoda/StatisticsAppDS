30b5b380928339e57907927502eaf52093f82d1e
DarianV <darian.voda00@e-uvt.ro>
2022-03-23 21:31:59 +0200

    Added library.R for development updates

---
+++ library.R
@@ -0,0 +1,12 @@
+getwd()
+setwd("C:/Users/daria/OneDrive/Desktop/Licenta/Diagram-Generator/Diagram.Generator")
+devtools::document()
+devtools::build_vignettes("introduction")
+devtools::install()
+library("Diagram.Generator")
+devtools::install_github(repo='DarianFlorianVoda/Diagram.Generator')
+usethis::gh_token_help()
+usethis::git_sitrep()
+
+usethis::edit_r_environ()
+gitcreds::gitcreds_set('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')
