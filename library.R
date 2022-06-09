63b796bc0285bde09c1864e2d206d2ba180d5065
Voda Darian <darian.voda00@e-uvt.ro>
2022-06-10 02:00:22 +0300

    load_all vs test()

--- library.R
+++ library.R
@@ -1,3 +1,4 @@
+library("devtools")
 getwd()
 setwd("C:/Users/daria/OneDrive/Desktop/Licenta/Diagram-Generator/Diagram.Generator")
 devtools::document()
@@ -10,3 +11,10 @@ usethis::git_sitrep()
 
 usethis::edit_r_environ()
 gitcreds::gitcreds_set('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')
+
+
+devtools::load_all()
+devtools::test()
+
+
+install.packages("testthat")
