6e37d66abf0249a5a27d4e2303970f45be0276e9
DarianV <darian.voda00@e-uvt.ro>
2022-06-24 11:36:01 +0300

    Issue #80

--- tests/testthat/test-ArrowDiamondHead.R
+++ tests/testthat/test-ArrowDiamondHead.R
@@ -77,9 +77,9 @@ linesAid(h1, h2, h3)
 
 cat("Test 2\n")
 # Total length = (d^2 + dV[1]^2) + (d^2 + dV[2]^2)
-testArrow(h=h1, d=d)
-testArrow(h=h2, d=d)
-testArrow(h=h3, d=d)
+testArrow(h=h1, d=d, dV=d.head)
+testArrow(h=h2, d=d, dV=d.head)
+testArrow(h=h3, d=d, dV=d.head)
 
 
 ###### Test 3 ######
@@ -90,13 +90,13 @@ scale = (100/12) * aspect_ratio_max;
 plot.base(ylim = c(0,100))
 a1 = arrowDiamond(x, y, d=d, d.head=d.head, lwd=2, scale=scale);
 a2 = arrowDiamond(x2, y2, d=d, d.head=d.head, lwd=2, scale=scale);
-# a3 = arrowDiamond(c(x[1], 9), c(y[1], y[1]), d=d, d.head=d.head, lwd=2, scale=scale);
+a3 = arrowDiamond(c(x[1], 9), c(y[1], y[1]), d=d, d.head=d.head, lwd=2, scale=scale);
 a4 = arrowDiamond(c(x[1], x[1]), c(y[1], 50), d=d, d.head=d.head, lwd=2, scale=scale);
 # Head
 h1 = a1$Head[[1]]
 h2 = a2$Head[[1]]
-# h3 = a3$Head[[1]]
+h3 = a3$Head[[1]]
 h4 = a4$Head[[1]]
 # - visual aids:
-linesAid(h1, h2, h4)
+linesAid(h1, h2, h3, h4)
 cat("Test 3: only visual\n\n")
