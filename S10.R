f442e35a2faff4373c1745214746d9b1af2c8899
MasterDari <73436590+DarianFlorianVoda@users.noreply.github.com>
2022-12-22 19:23:50 +0100

    Add files via upload

---
+++ S10.R
@@ -0,0 +1,189 @@
+# posterior mean
+posterior_mean = function(x, sigma, mu, tau){
+  mu_post = ((length(x)/(sigma*sigma))*mean(x) + (1/(tau*tau))) / ((length(x)/(sigma*sigma)) + (1/(tau*tau)))
+    
+  return(mu_post)
+}
+
+
+n = 10
+m = 100
+tau_dev = 10
+mu_priori = 5
+sigma_priori = 6
+
+priori_dist = rnorm(n, mean=mu_priori, sd=tau_dev)
+
+
+x = rnorm(m, mean = posterior_mean(priori_dist, sigma_priori, mu_priori, tau_dev), sd = sigma_priori)
+
+# posterior variance
+posterior_variance = function(x, sigma, mu, tau){
+  posterior_var = 1 / ( (length(x)/(sigma*sigma)) + (1/(tau*tau)))
+  
+  return(posterior_var)
+}
+
+
+
+# 1.2
+n_1 = 2
+
+
+
+
+##############################
+
+#####################
+##### Exercise 1 #####
+#####################
+
+
+
+post.mean = function(x,
+                     sigma, # Known standard deviation
+                     mu, # Prior mean
+                     tau){ # Prior standard deviation
+  n = length(x)
+  a = n/sigma^2
+  b = 1/tau^2
+  mu.hat = (a * mean(x) + b * mu)/(a+b)
+  return(mu.hat)
+}
+
+
+
+post.variance = function(x,
+                         sigma,
+                         mu,
+                         tau){
+  n = length(x)
+  a = n/sigma^2
+  b = 1/tau^2
+  sigma.squared.hat = (a+b)^-1
+  return(sigma.squared.hat )
+}
+
+
+#####################
+##### Exercise 2 #####
+#####################
+
+
+
+n = 2000
+x = rnorm(n, mean = 80, sd = 10)
+values = seq(0,100,0.1)
+plot(values, dnorm(values,
+                   mean = post.mean(x, sigma = 10, mu = 50, tau = sqrt(25)),
+                   sd = post.variance(x, sigma = 10, mu = 50, tau = sqrt(25))),
+     type = "l")
+
+
+
+#####################
+##### Exercise 3 #####
+#####################
+
+sigma_val = 15
+n = 500
+x = rnorm(n, mean = 200, sd = 100)
+values = c(185, 200, 225, 208, 194, 217, 220, 203, 206, 190)
+values2 = seq(0, 500, 0.1)
+plot(values2, dnorm(values2,
+                    mean = post.mean(x, sigma = 15, mu = 200, tau = sqrt(100)),
+                    sd = post.variance(x, sigma = 15, mu = 200, tau = sqrt(100))),
+     type = "l")
+
+
+1-pnorm(200, mean=post.mean(x, sigma = 15, mu = 200, tau = 100), sqrt(post.variance(x, sigma = 15, mu = 200, tau = 100)))
+
+
+##### Prof sol.
+
+#####################
+##### Exercise 3#####
+#####################
+values = seq(0,500,0.1)
+plot(values, dnorm(values, 200, 100), type = "l", col = "blue") # Prior density
+
+
+
+x=c(185,200,225,208,194,217,220,203,206,190)
+pMean = post.mean(x, sigma = 15, mu = 200, tau = 100) # Posterior mean
+pVariance = post.variance(x, sigma = 15, mu = 200, tau = 100) # Posterior variance
+plot(values, dnorm(values,
+                   mean = pMean,
+                   sd = sqrt(pVariance)),type = "l")
+
+
+
+1-pnorm(200, pMean, sqrt(pVariance)) # Probability unsafe to swim
+
+
+######################
+
+### Ex. 4
+
+n = 200
+x = rnorm(n, mean = 80, sd = 100)
+values = seq(0,100,0.1)
+plot(values, dnorm(values,
+                   mean = post.mean(x, sigma = 10, mu = 50, tau = sqrt(250)),
+                   sd = post.variance(x, sigma = 10, mu = 50, tau = sqrt(250))),
+     type = "l")
+
+line(values, dnorm(values,
+                   mean = post.mean(x, sigma = 10, mu = 50, tau = sqrt(2500)),
+                   sd = post.variance(x, sigma = 10, mu = 50, tau = sqrt(2500))))
+
+
+### prof solution
+
+#####################
+##### Exercise 4#####
+#####################
+
+
+
+# Convergence to improper prior
+tau.sqaured = 1
+values = seq(-50,50,0.1)
+plot(values, dnorm(values,
+                   mean = 0,
+                   sd = sqrt(tau.sqaured)),
+     type = "l",
+     col = "blue",
+     ylim = c(0,0.5))
+tau.sqaured = 100
+lines(values, dnorm(values,
+                    mean = 0,
+                    sd = sqrt(tau.sqaured)),
+      type = "l",
+      col = "red",
+      ylim = c(0,0.5))
+tau.sqaured = 10000
+lines(values, dnorm(values,
+                    mean = 0,
+                    sd = sqrt(tau.sqaured)),
+      type = "l",
+      col = "black",
+      ylim = c(0,0.5))
+legend("topright", legend=c("tau^2 = 1", "tau^2 = 100", "tau^2 = 10000"),
+       col=c("red", "blue", "black"), lty=1)
+
+
+
+# 1.3 with improper prior
+x=c(185,200,225,208,194,217,220,203,206,190)
+pMean = mean(x) # Posterior mean
+pVariance = 15^2/length(x) # Posterior variance
+values = seq(0,500,0.1)
+plot(values, dnorm(values,
+                   mean = pMean,
+                   sd = sqrt(pVariance)),type = "l")
+
+
+
+1-pnorm(200, pMean, sqrt(pVariance)) # Probability unsafe to swim
+
